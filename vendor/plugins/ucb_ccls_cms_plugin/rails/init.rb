a175ca5372eeea0b5ff72d28a6370a7736d23113
Jake <github@jake.otherinbox.com>
2010-07-12 13:19:42 -0700

    UCB CCLS CMS Engine update.

--- vendor/plugins/ucb_ccls_cms_plugin/rails/init.rb
+++ vendor/plugins/ucb_ccls_cms_plugin/rails/init.rb
@@ -5,7 +5,12 @@ config.load_paths << File.join(File.dirname(__FILE__),'..','/app/sweepers')
 
 config.gem "RedCloth"
 
-config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
+#	This works in the app's config/environment.rb ...
+#config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
+#	but apparently not here, so ...
+HTML::WhiteListSanitizer.allowed_attributes.merge(%w(
+	id class style
+))
 
 config.reload_plugins = true if RAILS_ENV == 'development'
 
