66b8e8bf1d93ca07a002adbab65850bd5ec605f9
Jake <github@jake.otherinbox.com>
2010-07-15 15:42:35 -0700

    Added crude versions of images and documents

--- test/factories.rb
+++ test/factories.rb
@@ -1,46 +1,3 @@
-Factory.define :document do |f|
-end
-
-Factory.define :image do |f|
-end
-
-Factory.define :home_page_pic do |f|
-	f.sequence(:title){ |n| "Title #{n}" }
-end
-
-Factory.define :page do |f|
-	f.sequence(:path) { |n| "/path#{n}" }
-	f.sequence(:menu_en) { |n| "Menu #{n}" }
-	f.sequence(:title_en){ |n| "Title #{n}" }
-	f.body_en  "Page Body"
-end
-
-Factory.define :role do |f|
-	f.sequence(:name) { |n| "name#{n}" }
-end
-
-Factory.define :user do |f|
-	f.sequence(:uid) { |n| "UID#{n}" }
-#	f.sequence(:username) { |n| "username#{n}" }
-#	f.sequence(:email) { |n| "username#{n}@example.com" }
-#	f.password 'V@1!dP@55w0rd'
-#	f.password_confirmation 'V@1!dP@55w0rd'
-#	f.role_name 'user'
-end
-Factory.define :admin_user, :parent => :user do |f|
-	f.administrator true
-end	#	parent must be defined first
-Factory.define :sender, :parent => :user do |f|
-#	This is really just an alias of convenience for UserInvitation
-end	#	parent must be defined first
-
-Factory.define :user_invitation do |f|
-	f.association :sender, :factory => :user
-	f.sequence(:email){|n| "invitation#{n}@example.com"}
-end
-
-
-
 Factory.define :group do |f|
 	f.sequence(:name) { |n| "Name#{n}" }
 end
@@ -61,4 +18,3 @@ Factory.define :post do |f|
 	f.association :user
 	f.sequence(:body) { |n| "Body #{n}" }
 end
-
