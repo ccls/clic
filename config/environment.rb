3c2d115f9f0c186b47244559d8babbaf0c219283
Jake <github@jake.otherinbox.com>
2010-07-19 12:33:17 -0700

    Added 2 prefix handlers for RedCloth parsing.

--- config/environment.rb
+++ config/environment.rb
@@ -76,3 +76,30 @@ Rails::Initializer.run do |config|
 	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
 	# config.i18n.default_locale = :de
 end
+
+
+#	Not quite worthy of its own file. (yet)
+module RedCloth::Formatters::HTML
+
+	def link_with_prefix(opts)
+		if opts[:href] =~ /^\//
+			opts[:href] = ActionController::Base.relative_url_root + opts[:href]
+		end
+		link_without_prefix(opts)
+	end
+	alias_method_chain :link, :prefix
+
+	def image_with_prefix(opts)
+		if opts[:href] && opts[:href] =~ /^\//
+			opts[:href] = ActionController::Base.relative_url_root + opts[:href]
+		end
+		if opts[:src] =~ /^\//
+			opts[:src] = ActionController::Base.relative_url_root + opts[:src]
+		end
+		image_without_prefix(opts)
+	end
+	alias_method_chain :image, :prefix
+
+end
+
+
