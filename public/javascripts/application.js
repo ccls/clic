a2a69c3028795a4c7bd952bfe5fdc1609382b13b
Jake <github@jake.otherinbox.com>
2010-07-26 13:53:25 -0700

    Moved javascript code into template.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,23 +13,13 @@ jQuery(function(){
 		location.pathname.replace(new RegExp('^' + root),'') );
 */
 
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-17648061-7']);
-	_gaq.push(['_trackPageview']);
 
-	var ga = document.createElement('script'); 
-	ga.type = 'text/javascript'; 
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 
-		'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; 
-	s.parentNode.insertBefore(ga, s);
-
-	var ga = document.createElement('script'); 
-	ga.type = 'text/javascript'; 
-	ga.async = true;
-	ga.src = "http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en";
-	var s = document.getElementsByTagName('script')[0]; 
-	s.parentNode.insertBefore(ga, s);
-
+	(function(){
+		var ga = document.createElement('script'); 
+		ga.type = 'text/javascript'; 
+		ga.async = true;
+		ga.src = "http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en";
+		var s = document.getElementsByTagName('script')[0]; 
+		s.parentNode.insertBefore(ga, s);
+	})();
 });
