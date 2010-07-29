b502de8e19513d1670300c8012779b3087f6029d
Jake <github@jake.otherinbox.com>
2010-07-29 12:17:55 -0700

    Removed a lot of unused commented code.

--- config/environment.rb
+++ config/environment.rb
@@ -14,22 +14,10 @@ require File.join(File.dirname(__FILE__), 'boot')
 RAILS_APP_NAME = 'clic'
 
 Rails::Initializer.run do |config|
-	# Settings in config/environments/* take precedence over those specified here.
-	# Application configuration should go into files in config/initializers
-	# -- all .rb files in that directory are automatically loaded.
-
-	# Add additional load paths for your own custom dirs
-	# config.load_paths += %W( #{RAILS_ROOT}/extras )
-#	config.load_paths << "#{RAILS_ROOT}/app/sweepers"
-
-	# Specify gems that this application depends on and have them installed with rake gems:install
-	# config.gem "bj"
-	# config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-	# config.gem "sqlite3-ruby", :lib => "sqlite3"
-	# config.gem "aws-s3", :lib => "aws/s3"
-
 
 	#	This is set to /clic in environments/production.rb
+	#	As this app may be accessed via clic.berkeley.edu,
+	#	it MAY remain blank in production.
 	config.action_controller.relative_url_root = ''
 	
 	if RUBY_PLATFORM =~ /java/
@@ -41,10 +29,6 @@ Rails::Initializer.run do |config|
 		#	Additional jruby specific jars required in the war
 		config.gem 'jdbc-sqlite3', :lib => 'jdbc/sqlite3'
 		config.gem 'jruby-openssl', :lib => 'openssl'
-#
-#	I don't remember why we use haml?  surveyor?
-#
-#		config.gem 'haml'
 	else
 
 		# If using mysql ...
@@ -61,36 +45,24 @@ Rails::Initializer.run do |config|
 	end
 
 	config.gem "chronic"   #		http://chronic.rubyforge.org/
-#	config.gem "packet"    #		http://packet.rubyforge.org/
 	config.gem 'will_paginate'
 	config.gem 'fastercsv'
 
 
-	# Only load the plugins named here, in the order given (default is alphabetical).
-	# :all can be used as a placeholder for all plugins not explicitly named
 	# config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-	# Skip frameworks you're not going to use. To use Rails without a database,
-	# you must remove the Active Record framework.
-	# config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 	config.frameworks -= [ :active_resource ]
-
-	# Activate observers that should always be running
-	# config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
 	# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
 	# Run "rake -D time" for a list of tasks for finding time zone names.
 	config.time_zone = 'UTC'
 
-	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-	# config.i18n.default_locale = :de
 end
 
 
 #	Not quite worthy of its own file. (yet)
 #
-# Move me into engine.
+# Move me into engine.  And then clean this up a bit.
 module RedCloth::Formatters::HTML
 
 	def link_with_prefix(opts)
