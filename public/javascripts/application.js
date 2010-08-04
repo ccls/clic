2ada277ba96bc19b76258fb220ee41c30dd513e2
Jake <github@jake.otherinbox.com>
2010-08-04 14:32:51 -0700

    Added some comments and removing siteurl.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,5 +15,22 @@ http://www.google.com/cse/docs/cref.html
 		ga.src = "http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en";
 		var s = document.getElementsByTagName('script')[0]; 
 		s.parentNode.insertBefore(ga, s);
+		/*
+			This script adds the google watermark to the search box,
+			as well as adding a 'siteurl' field to the form.
+		$('#cse-search-box input[name=siteurl]').remove();
+		*/
 	})();
+
+/*
+	If a user make many multiple searches, the siteurl will
+	continue to be appended to, which is just irritating.
+	Unfortunately, I can't really stop google from doing it.
+	Actually, we could by copying in the 'brand' javascript,
+	and just removing that part.
+*/
+	jQuery('form#cse-search-box').submit(function(){
+		jQuery('form#cse-search-box input[name=siteurl]').remove();
+	});
+
 });
