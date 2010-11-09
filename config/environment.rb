4ccb323513a03c1bd1e42c44a1ec8b6dd734f3f8
Jake <github@jake.otherinbox.com>
2010-11-09 11:43:05 -0800

    Reran ccls_engine generator.

--- config/environment.rb
+++ config/environment.rb
@@ -67,24 +67,3 @@ Rails::Initializer.run do |config|
 	config.time_zone = 'UTC'
 
 end
-
-#	Due to some flexibility mods to CalnetAuthenticated
-#	User doesn't load until it is needed, 
-#	which means that calnet_authenticated isn't called,
-#	which means that current_user doesn't know what 
-#	a User is yet, which causes lots of ...
-#	NoMethodError (undefined method `find_create_and_update_by_uid' for nil:NilClass):
-#	so ...
-#require 'user' unless defined?(User)
-
-if Gem.searcher.find('ccls_engine')
-require 'ccls_engine' # without this, rake has problems ...
-#undefined local variable or method `ucb_authenticated' for #<Class:0x1059408    d0>
-#require 'simply_documents'
-require 'user' unless defined?(User)
-#require 'role' unless defined?(Role)
-end
-
-#	Actually, this is probably only needed in development,
-#	but putting it in environments/development.rb doesn't
-#	work right, even in an after_initialize.
