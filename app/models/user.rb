2e87cae26a6b0ef20838df1582bca8e3ec820cc0
Jake <github@jakewendt.com>
2011-02-22 11:05:08 -0800

    Not using calnet_authenticated anymore.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
+#if g = Gem.source_index.find_name('ccls-ccls_engine').last
+#require 'ccls_engine'
+#require g.full_gem_path + '/app/models/user'
+#end
+#
+#User.class_eval do
 #	== requires
 #	*	uid (unique)
 #
@@ -6,11 +12,12 @@
 #	*	displayname
 #	*	mail
 #	*	telephonenumber
-class User < ActiveRecord::Base
+#class User < ActiveRecord::Base
+class User < Ccls::User
 
 	has_and_belongs_to_many :groups
 
-	ucb_authenticated
+#	ucb_authenticated
 	document_owner
 
 #	alias_method :may_view_calendar?, :may_read?
