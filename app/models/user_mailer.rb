a57f2e7f746078a9edc6d5b794479cef63990b72
Jake <github@jakewendt.com>
2011-03-21 15:39:52 -0700

    Removed tabs from alignment.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,28 +1,28 @@
 class UserMailer < ActionMailer::Base
 
 	def new_user_email(user,sent_at = Time.now)
-		subject		 'New CLIC User'
+		subject    'New CLIC User'
 		recipients 'clic_centraloffice@berkeley.edu'
-		from			 ''
-		sent_on		 sent_at
+		from       ''
+		sent_on    sent_at
 		body       :user => user
 	end
 
 	def confirm_email(user,sent_at = Time.now)
-		subject		 'Email Confirmation'
+		subject    'Email Confirmation'
 		recipients user.email
-		from			 ''
-		sent_on		 sent_at
-		body			 :greeting => 'Hi,'
-		body			 :confirm_url => confirm_email_url(user.perishable_token)
+		from       ''
+		sent_on    sent_at
+		body       :greeting => 'Hi,'
+		body       :confirm_url => confirm_email_url(user.perishable_token)
 	end
 
 #	def forgot_password(sent_at = Time.now)
-#		subject		'UserMailer#forgot_password'
+#		subject    'UserMailer#forgot_password'
 #		recipients ''
-#		from			 ''
-#		sent_on		sent_at
-#		body			 :greeting => 'Hi,'
+#		from       ''
+#		sent_on    sent_at
+#		body       :greeting => 'Hi,'
 #	end
 
 end
