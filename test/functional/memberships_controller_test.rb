6116d9fe7cafd965a04f1e205fdc01ebefb4a484
Jake <github@jakewendt.com>
2011-03-22 17:02:07 -0700

    Added missing memberships tests.

--- test/functional/memberships_controller_test.rb
+++ test/functional/memberships_controller_test.rb
@@ -30,4 +30,48 @@ class MembershipsControllerTest < ActionController::TestCase
 	assert_access_with_https
 	assert_no_access_with_http
 
+	%w( superuser admin ).each do |cu|
+	
+		test "should NOT edit membership with invalid id and #{cu} login" do
+			login_as send(cu)
+			get :edit, :id => 0
+			assert_not_nil flash[:error]
+			assert_redirected_to memberships_path
+		end
+	
+		test "should NOT update membership with invalid id and #{cu} login" do
+			login_as send(cu)
+			put :update, :id => 0
+			assert_not_nil flash[:error]
+			assert_redirected_to memberships_path
+		end
+	
+		test "should approve membership with #{cu} login and no params" do
+			membership = create_membership(:approved => false)
+			login_as send(cu)
+			assert !membership.approved?
+			put :update, :id => membership.id
+			assert  membership.reload.approved?
+			assert_redirected_to memberships_path
+		end
+	
+		test "should update membership group role with #{cu} login and new role" do
+			create_a_membership
+			login_as send(cu)
+			assert_equal @membership.group_role, GroupRole['reader']
+			put :update, :id => @membership.id, :membership => { 
+				:group_role_id => GroupRole['editor'] }
+			assert_equal @membership.reload.group_role, GroupRole['editor']
+			assert_redirected_to memberships_path
+		end
+	
+		test "should NOT destroy membership with invalid id and #{cu} login" do
+			login_as send(cu)
+			delete :destroy, :id => 0
+			assert_not_nil flash[:error]
+			assert_redirected_to memberships_path
+		end
+	
+	end
+
 end
