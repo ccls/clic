edc457124cb13ff702082b3bb65df08a6c01fd21
Jake <github@jakewendt.com>
2011-04-15 10:35:21 -0700

    Prepping to use TinyMCE for editing textareas.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -40,4 +40,29 @@ http://www.google.com/cse/docs/cref.html
 		jQuery(this).next().toggleClass('ui-icon-triangle-1-s');
 		return false;
 	});
+
+
+	if( typeof jQuery('textarea.tinymce').tinymce == 'function' ){
+		jQuery('textarea.tinymce').tinymce({
+			// Location of TinyMCE script
+			script_url : root + '/javascripts/tiny_mce/tiny_mce.js',
+	
+			// General options
+			theme : "advanced",
+	
+			// Theme options
+			theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,hr,link",
+			theme_advanced_buttons2 : '',	// otherwise will use defaults
+			theme_advanced_buttons3 : '',	// otherwise will use defaults
+			theme_advanced_statusbar_location : "bottom",
+			theme_advanced_toolbar_location : "top",
+			theme_advanced_toolbar_align : "left",
+//			theme_advanced_resizing : true,
+	
+			// Example content CSS (should be your site CSS)
+			content_css : root + "/stylesheets/layout.css",
+	
+		});
+	}
+
 });
