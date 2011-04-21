a488988b5f582b19cea1ca829c6ba9670c4d57c4
Jake <github@jakewendt.com>
2011-04-21 12:08:10 -0700

    Added tests for editor assisting controllers.

--- test/functional/editor_images_controller_test.rb
+++ test/functional/editor_images_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class EditorImagesControllerTest < ActionController::TestCase
-  test "the truth" do
-    pending
+
+	assert_no_access_without_login({
+		:actions => [:index]
+	})
+
+	assert_no_route(:get,:new)
+	assert_no_route(:post,:create)
+	assert_no_route(:get, :show)
+	assert_no_route(:get, :edit)
+	assert_no_route(:put, :update)
+	assert_no_route(:delete, :destroy)
+
+	setup :create_a_membership
+
+	ALL_TEST_ROLES.each do |cu|
+	
+ 	 test "should return index.js with #{cu} login" do
+			login_as send(cu)
+			get :index, :format => :js
+			assert_response :success
+			assert_template 'index'
+			assert assigns(:photos)
+			assert_match /var tinyMCEImageList = new Array/, @response.body
+	  end
+
   end
+
 end
--- test/functional/editor_links_controller_test.rb
+++ test/functional/editor_links_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class EditorLinksControllerTest < ActionController::TestCase
-  test "the truth" do
-    pending
+
+	assert_no_access_without_login({
+		:actions => [:index]
+	})
+
+	assert_no_route(:get,:new)
+	assert_no_route(:post,:create)
+	assert_no_route(:get, :show)
+	assert_no_route(:get, :edit)
+	assert_no_route(:put, :update)
+	assert_no_route(:delete, :destroy)
+
+	setup :create_a_membership
+
+	ALL_TEST_ROLES.each do |cu|
+	
+ 	 test "should return index.js with #{cu} login" do
+			login_as send(cu)
+			get :index, :format => :js
+			assert_response :success
+			assert_template 'index'
+			assert assigns(:pages)
+			assert_match /var tinyMCELinkList = new Array/, @response.body
+	  end
+
   end
+
 end
