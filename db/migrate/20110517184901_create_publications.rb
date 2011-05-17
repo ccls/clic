53fa3429e1a26618bf8e1a52155d72067514998d
Jake <github@jakewendt.com>
2011-05-17 11:50:46 -0700

    Added Publication model.[#34]

---
+++ db/migrate/20110517184901_create_publications.rb
@@ -0,0 +1,19 @@
+class CreatePublications < ActiveRecord::Migration
+	def self.up
+		create_table :publications do |t|
+			t.integer :publication_subject_id
+			t.integer :study_id
+			t.string :author_last_name
+			t.string :publication_year
+			t.string :journal
+			t.string :title
+			t.timestamps
+		end
+		add_index :publications, :publication_subject_id
+		add_index :publications, :study_id
+	end
+
+	def self.down
+		drop_table :publications
+	end
+end
