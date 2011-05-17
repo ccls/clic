0a5c66bcaef5c5ef5cd1f4f91389e101af11febc
Jake <github@jakewendt.com>
2011-05-17 12:58:37 -0700

    Sorted factories.

--- test/factories.rb
+++ test/factories.rb
@@ -1,3 +1,91 @@
+Factory.define :announcement do |f|
+	f.association :user
+	f.sequence(:title) { |n| "Announcement Title #{n}" }
+	f.content "Some announcement content"
+end
+Factory.define :group_announcement, :parent => :announcement do |f|
+	f.association :group
+end
+
+Factory.define :annual_meeting do |f|
+	f.sequence(:meeting) { |n| "Meeting #{n}" }
+	f.sequence(:abstract) { |n| "Abstract #{n}" }
+end
+
+Factory.define :document do |f|
+	f.sequence(:title) { |n| "Document Title #{n}" }
+#	f.sequence(:document_file_name) { |n| "document_file_name#{n}" }
+end
+
+Factory.define :doc_form do |f|
+	f.sequence(:title) { |n| "Title #{n}" }
+	f.sequence(:abstract) { |n| "Abstract #{n}" }
+end
+
+Factory.define :event do |f|
+	f.association :user
+	f.sequence(:title) { |n| "Event Title #{n}" }
+	f.content "Some event content"
+	f.begins_on Chronic.parse('tomorrow')
+end
+Factory.define :group_event, :parent => :event do |f|
+	f.association :group
+end
+
+Factory.define :forum do |f|
+#	f.association :group
+	f.sequence(:name) { |n| "Forum #{n}" }
+end
+
+Factory.define :group do |f|
+	f.sequence(:name) { |n| "Group #{n}" }
+end
+
+Factory.define :group_document do |f|
+	f.association :post
+	f.association :user
+#	f.association :group
+	f.sequence(:title) { |n| "Group Document Title #{n}" }
+end
+
+Factory.define :group_role do |f|
+	f.sequence(:name) { |n| "Group Role #{n}" }
+end
+
+Factory.define :membership do |f|
+	f.association :user
+	f.association :group
+#	f.association :group_role
+	f.updated_at Time.now	#	to make it dirty
+end
+
+Factory.define :post do |f|
+	f.association :topic
+	f.association :user
+	f.sequence(:body) { |n| "Post Body #{n}" }
+end
+
+Factory.define :publication do |f|
+	f.sequence(:title) { |n| "Title #{n}" }
+	f.sequence(:journal) { |n| "Journal #{n}" }
+	f.sequence(:publication_year) { |n| "Publication Year #{n}" }
+	f.sequence(:author_last_name) { |n| "Author Last Name #{n}" }
+end
+
+Factory.define :publication_subject do |f|
+	f.sequence(:name) { |n| "Name #{n}" }
+end
+
+Factory.define :study do |f|
+	f.sequence(:name) { |n| "Name #{n}" }
+end
+
+Factory.define :topic do |f|
+	f.association :forum
+	f.association :user
+	f.sequence(:title) { |n| "Topic Title #{n}" }
+end
+
 Factory.define :user do |f|
 	f.sequence(:username) { |n| "username#{n}" }
 	f.sequence(:email) { |n| "username#{n}@example.com" }
@@ -15,85 +103,3 @@ Factory.define :user do |f|
 	f.phone_number "PhoneNumber"
 end
 
-Factory.define :group do |f|
-	f.sequence(:name) { |n| "Group #{n}" }
-end
-
-Factory.define :forum do |f|
-#	f.association :group
-	f.sequence(:name) { |n| "Forum #{n}" }
-end
-
-Factory.define :topic do |f|
-	f.association :forum
-	f.association :user
-	f.sequence(:title) { |n| "Topic Title #{n}" }
-end
-
-Factory.define :post do |f|
-	f.association :topic
-	f.association :user
-	f.sequence(:body) { |n| "Post Body #{n}" }
-end
-
-Factory.define :group_role do |f|
-	f.sequence(:name) { |n| "Group Role #{n}" }
-end
-
-Factory.define :membership do |f|
-	f.association :user
-	f.association :group
-#	f.association :group_role
-	f.updated_at Time.now	#	to make it dirty
-end
-
-Factory.define :document do |f|
-	f.sequence(:title) { |n| "Document Title #{n}" }
-#	f.sequence(:document_file_name) { |n| "document_file_name#{n}" }
-end
-
-Factory.define :announcement do |f|
-	f.association :user
-	f.sequence(:title) { |n| "Announcement Title #{n}" }
-	f.content "Some announcement content"
-end
-Factory.define :group_announcement, :parent => :announcement do |f|
-	f.association :group
-end
-Factory.define :event do |f|
-	f.association :user
-	f.sequence(:title) { |n| "Event Title #{n}" }
-	f.content "Some event content"
-	f.begins_on Chronic.parse('tomorrow')
-end
-Factory.define :group_event, :parent => :event do |f|
-	f.association :group
-end
-Factory.define :group_document do |f|
-	f.association :post
-	f.association :user
-#	f.association :group
-	f.sequence(:title) { |n| "Group Document Title #{n}" }
-end
-
-
-Factory.define :publication_subject do |f|
-	f.sequence(:name) { |n| "Name #{n}" }
-end
-Factory.define :study do |f|
-	f.sequence(:name) { |n| "Name #{n}" }
-end
-Factory.define :doc_form do |f|
-	f.sequence(:title) { |n| "Title #{n}" }
-	f.sequence(:abstract) { |n| "Abstract #{n}" }
-end
-Factory.define :publication do |f|
-	f.sequence(:title) { |n| "Title #{n}" }
-	f.sequence(:journal) { |n| "Journal #{n}" }
-	f.sequence(:publication_year) { |n| "Publication Year #{n}" }
-	f.sequence(:author_last_name) { |n| "Author Last Name #{n}" }
-end
-Factory.define :annual_meeting do |f|
-	f.sequence(:meeting) { |n| "Meeting #{n}" }
-	f.sequence(:abstract) { |n| "Abstract #{n}" }
-end
