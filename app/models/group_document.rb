8d7bea1bf28a97151fe0a83b5eed2b6239ff293e
Jake <github@jakewendt.com>
2011-06-17 15:28:05 -0700

    Comment that group document will need to polymorphicize belongs_to association.

--- app/models/group_document.rb
+++ app/models/group_document.rb
@@ -3,9 +3,9 @@ require 'hmac-sha1'
 #	http://amazon.rubyforge.org/
 #
 class GroupDocument < ActiveRecord::Base
-	belongs_to :group
+	belongs_to :group	#	sometimes
 	belongs_to :user
-	belongs_to :post
+	belongs_to :post	#	TODO gonna need to polymorphicize this
 
 	validates_presence_of :user, :post, :title
 	validates_length_of :title,       :maximum => 250
