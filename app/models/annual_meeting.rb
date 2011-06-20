5d5fbc55127fa2216825355dcc7658eb10dc87e2
Jake <github@jakewendt.com>
2011-06-20 11:42:36 -0700

    Added group_documents association to annual_meeting.[#35]

--- app/models/annual_meeting.rb
+++ app/models/annual_meeting.rb
@@ -1,5 +1,9 @@
 class AnnualMeeting < ActiveRecord::Base
 
+	has_many   :group_documents, :dependent => :destroy, :as => :attachable
+
+# TODO accepts_nested_attributes_for :group_documents
+
 	validates_presence_of :meeting, :abstract
 	validates_length_of :meeting,  :maximum => 250
 	validates_length_of :abstract, :maximum => 65000
--- test/unit/annual_meeting_test.rb
+++ test/unit/annual_meeting_test.rb
@@ -6,6 +6,7 @@ class AnnualMeetingTest < ActiveSupport::TestCase
 	assert_should_require(:meeting, :abstract)
 	assert_should_require_attribute_length( :meeting,  :maximum => 250 )
 	assert_should_require_attribute_length( :abstract, :maximum => 65000 )
+	assert_should_have_many(:group_documents, :as => :attachable)
 
 	test "should return meeting as to_s" do
 		object = create_object
