78916e51caa9e706e03df5517e6aa46ab80f3c91
Jake <github@jakewendt.com>
2011-06-20 11:24:29 -0700

    Added group_documents association to doc_form.[#40]

--- app/models/doc_form.rb
+++ app/models/doc_form.rb
@@ -4,6 +4,10 @@ class DocForm < ActiveRecord::Base
 	validates_length_of   :title,    :maximum => 250
 	validates_length_of   :abstract, :maximum => 65000
 
+	has_many   :group_documents, :dependent => :destroy, :as => :attachable
+
+# TODO accepts_nested_attributes_for :group_documents
+
 	def to_s
 		title
 	end
--- test/unit/doc_form_test.rb
+++ test/unit/doc_form_test.rb
@@ -4,9 +4,10 @@ class DocFormTest < ActiveSupport::TestCase
 
 	assert_should_create_default_object
 	assert_should_require(:title, :abstract)
-
 	assert_should_require_attribute_length( :title,    :maximum => 250 )
 	assert_should_require_attribute_length( :abstract, :maximum => 65000 )
+
+	assert_should_have_many(:group_documents, :as => :attachable)
 
 	test "should return title as to_s" do
 		object = create_object
