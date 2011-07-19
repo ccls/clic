12091ac6e54dcd32fd078d31e1855a4205db1d5f
Jake <github@jakewendt.com>
2011-07-19 13:50:42 -0700

    Force chronic version 0.5.0 as 0.6.0 doesn't parse properly.

--- config/environment.rb
+++ config/environment.rb
@@ -52,7 +52,8 @@ Rails::Initializer.run do |config|
 	#	require it, but don't load it
 	config.gem 'jakewendt-rdoc_rails', :lib => false
 
-	config.gem "chronic"   #		http://chronic.rubyforge.org/
+	#		http://chronic.rubyforge.org/
+	config.gem "chronic", :version => '= 0.5.0'
 	config.gem 'will_paginate'
 	config.gem 'fastercsv'
 	config.gem 'hpricot'
