69400a28299ad6ecfc046a6c283ef803a9c8d8b9
Jake <github@jakewendt.com>
2012-04-17 14:41:56 -0700

    Added directory search tests.[#96]

--- test/functional/directories_controller_test.rb
+++ test/functional/directories_controller_test.rb
@@ -12,6 +12,61 @@ class DirectoriesControllerTest < ActionController::TestCase
 			get :show
 			assert_response :success
 			assert_template 'show'
+			assert !assigns(:members).empty?
+		end
+
+		test "should search directory by bogus first name with #{cu} login" do
+			login_as send(cu)
+			get :show, :first_name => 'bogus'
+			assert_response :success
+			assert_template 'show'
+			assert assigns(:members).empty?
+		end
+
+		test "should search directory by first name with #{cu} login" do
+			user = send(cu)
+			assert_not_nil user.first_name
+			login_as user
+			get :show, :first_name => user.first_name
+			assert_response :success
+			assert_template 'show'
+			assert !assigns(:members).empty?
+		end
+
+		test "should search directory by bogus last name with #{cu} login" do
+			login_as send(cu)
+			get :show, :last_name => 'bogus'
+			assert_response :success
+			assert_template 'show'
+			assert assigns(:members).empty?
+		end
+
+		test "should search directory by last name with #{cu} login" do
+			user = send(cu)
+			assert_not_nil user.last_name
+			login_as user
+			get :show, :last_name => user.last_name
+			assert_response :success
+			assert_template 'show'
+			assert !assigns(:members).empty?
+		end
+
+		test "should search directory by bogus profession id with #{cu} login" do
+			login_as send(cu)
+			get :show, :profession_id => 'bogus'
+			assert_response :success
+			assert_template 'show'
+			assert assigns(:members).empty?
+		end
+
+		test "should search directory by profession id with #{cu} login" do
+			user = send(cu)
+			assert_not_nil user.profession_ids.first
+			login_as user
+			get :show, :profession_id => user.profession_ids.first
+			assert_response :success
+			assert_template 'show'
+			assert !assigns(:members).empty?
 		end
 
 	end
