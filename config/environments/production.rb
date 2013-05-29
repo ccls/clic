d00632661cfcb2d08e974df9519fe632a77c1b83
Jake <github@jakewendt.com>
2013-05-29 11:16:17 -0700

    Log more stuff in production for the moment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,10 @@ Clic::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+#	just until everything seems to be working
+config.log_level = :debug
+
+
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
